be0998d487a483cebe2004280aaa048b99bcb7cb
Cyrus Li <70495046+Sothis-baka@users.noreply.github.com>
2021-04-05 01:51:21 -0500

    make it better

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const resolvers = require('./graphql/resolvers');
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req} ) => ({ req })
+    context: ({ req} ) => ({ req }),
+    introspection: true,
+    playground: true,
 });
 
 mongoose
